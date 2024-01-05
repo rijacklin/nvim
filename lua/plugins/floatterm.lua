6a927dbaf75739336916826f22500e4f922338f9
rijacklin <rijacklin1@gmail.com>
2024-01-05 14:58:05 -0500

    Updated macos-specific config

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -2,7 +2,7 @@ return {
 		"voldikss/vim-floaterm",
 		-- FloaTerm configuration
 		config = function()
-				vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
+				vim.keymap.set('n', "tt", ":FloatermToggle myfloat<CR>")
 				vim.keymap.set('t', "<Esc>", "<C-\\><C-n>:q<CR>", { silent = true })
 		end,
 }
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,24 @@
 return {
-		'ThePrimeagen/harpoon',
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} },
+	config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
+
+		vim.keymap.set("n", "<leader>yy", function() harpoon:list():append() end)
+		vim.keymap.set("n", "<leader>yf", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(4) end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<leader>n", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
+	end
 }
